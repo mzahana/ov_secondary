f8380aef307ee503f05b6f92b66bb6f7a5ee409f
Mohamed Abdelkader <mohamedashraf123@gmail.com>
2024-05-28 20:17:53 +0100

    Support ros noetic - OpenCV 3

--- loop_fusion/src/utility/Grider_FAST.h
+++ loop_fusion/src/utility/Grider_FAST.h
@@ -33,7 +33,8 @@
 #include <Eigen/Eigen>
 
 
-#include <opencv/cv.hpp>
+//#include <opencv/cv.hpp>
+#include <opencv2/opencv.hpp>
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 
